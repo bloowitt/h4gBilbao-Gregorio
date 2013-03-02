42c246c52c2e9112bdf47cd4bffaf13a9165cfc7
Alex Ortiz de Guinea <bloowitt@gmail.com>
2013-03-02 10:13:30 +0100

    initial setup
    Just files for our app

---
+++ WebContent/phraseDatabase.js
@@ -0,0 +1,5 @@
+var phraseDb={
+		ocio:["quiero ver televisión","quiero matar una vaca","apaga la televisión"],
+		salud:["me duele la cabeza","creo que necesito ir al médico"],
+		higiene:["quiero ir al baño","necesito lavarme"]
+};
